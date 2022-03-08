785ea2ff82e68bccef75016f187d272349fb9c7e
PeshX <62329858+PeshX@users.noreply.github.com>
2022-03-08 11:10:00 +0100

    example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,174 @@
+import 'dart:async';
 import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_gauges/gauges.dart';
+import 'package:usb_serial/transaction.dart';
 import 'package:usb_serial/usb_serial.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class _MyAppState extends State<MyApp> {
+  UsbPort? _port;
+  String _status = "Idle";
+  List<Widget> _ports = [];
+  List<Widget> _serialData = [];
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  StreamSubscription<String>? _subscription;
+  Transaction<String>? _transaction;
+  UsbDevice? _device;
+
+  TextEditingController _textController = TextEditingController();
+
+  Future<bool> _connectTo(device) async {
+    _serialData.clear();
+
+    if (_subscription != null) {
+      _subscription!.cancel();
+      _subscription = null;
+    }
+
+    if (_transaction != null) {
+      _transaction!.dispose();
+      _transaction = null;
+    }
+
+    if (_port != null) {
+      _port!.close();
+      _port = null;
+    }
+
+    if (device == null) {
+      _device = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (await (_port!.open()) != true) {
+      setState(() {
+        _status = "Failed to open port";
+      });
+      return false;
+    }
+    _device = device;
+
+    await _port!.setDTR(true);
+    await _port!.setRTS(true);
+    await _port!.setPortParameters(
+        115200, UsbPort.DATABITS_8, UsbPort.STOPBITS_1, UsbPort.PARITY_NONE);
+
+    _transaction = Transaction.stringTerminated(
+        _port!.inputStream as Stream<Uint8List>, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction!.stream.listen((String line) {
+      setState(() {
+        _serialData.add(Text(line));
+        if (_serialData.length > 20) {
+          _serialData.removeAt(0);
+        }
+      });
+    });
+
+    setState(() {
+      _status = "Connected";
+    });
+    return true;
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    if (!devices.contains(_device)) {
+      _connectTo(null);
+    }
+    print(devices);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    devices.forEach((device) {
+      _ports.add(ListTile(
+          leading: Icon(Icons.usb),
+          title: Text(device.productName!),
+          subtitle: Text(device.manufacturerName!),
+          trailing: ElevatedButton(
+            child: Text(_device == device ? "Disconnect" : "Connect"),
+            onPressed: () {
+              _connectTo(_device == device ? null : device).then((res) {
+                _getPorts();
+              });
+            },
+          )));
+    });
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+    setState(() {
+      print(_ports);
+    });
+  }
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
 
-class _MyHomePageState extends State<MyHomePage> {
+    UsbSerial.usbEventStream!.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _connectTo(null);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            body: Center(
-                child: SfLinearGauge(
-      barPointers: [LinearBarPointer(value: 40)],
-    ))));
+      appBar: AppBar(
+        title: const Text('USB Serial Plugin example app'),
+      ),
+      body: Center(
+          child: Column(children: <Widget>[
+        Text(
+            _ports.length > 0
+                ? "Available Serial Ports"
+                : "No serial devices available",
+            style: Theme.of(context).textTheme.headline6),
+        ..._ports,
+        Text('Status: $_status\n'),
+        Text('info: ${_port.toString()}\n'),
+        ListTile(
+          title: TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Text To Send',
+            ),
+          ),
+          trailing: ElevatedButton(
+            child: Text("Send"),
+            onPressed: _port == null
+                ? null
+                : () async {
+                    if (_port == null) {
+                      return;
+                    }
+                    String data = _textController.text + "\r\n";
+                    await _port!.write(Uint8List.fromList(data.codeUnits));
+                    _textController.text = "";
+                  },
+          ),
+        ),
+        Text("Result Data", style: Theme.of(context).textTheme.headline6),
+        ..._serialData,
+      ])),
+    ));
   }
 }
